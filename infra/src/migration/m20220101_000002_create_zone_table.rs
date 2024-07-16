ee082d551133ccdbb3b2a85f4fa5aad57b3b21a4
mijinko <kougouseikeidansi@gmail.com>
2024-07-16 14:18:18 +0000

    c

--- infra/src/migration/m20220101_000002_create_zone_table.rs
+++ infra/src/migration/m20220101_000002_create_zone_table.rs
@@ -6,8 +6,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        // todo!();
 
         manager
             .create_table(
--- infra/src/migration/mod.rs
+++ infra/src/migration/mod.rs
@@ -4,7 +4,6 @@ pub use sea_orm_migration::prelude::*;
 use crate::DATABASE_URL;
 
 mod m20220101_000002_create_zone_table;
-mod m20220101_000003_create_wwn_table;
 mod m20220101_000004_create_zone_configuration_table;
 mod m20220101_000005_create_connected_server_table;
 
